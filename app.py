348b3d451f630c45801432de8b35e98d0467c8b5
bbigmic <122184799+bbigmic@users.noreply.github.com>
2024-12-09 18:43:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -473,4 +473,4 @@ def uploaded_file(filename):
 # Inicjalizacja bazy danych i uruchomienie aplikacji
 if __name__ == '__main__':
     with app.app_context():
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000)
